d6b9f171f8d885889a7f98a9b64ad21b1031f05b
Niklas Netter (gatoniel) <niknett@gmail.com>
2020-06-12 19:11:26 +0200

    corrected add_bin_path.sh

--- add_bin_path.sh
+++ add_bin_path.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# copied from
+# https://stackoverflow.com/a/50305652
+ipython profile create
 cat >/home/jovyan/.ipython/profile_default/ipython_config.py <<EOF
 c.InteractiveShellApp.exec_lines = [
     'import sys; sys.path.append("/home/netter/bin")'
